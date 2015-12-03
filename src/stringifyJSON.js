784c0948c3d72d094ff659f1821f7e687c0ef0ba
edulis8 <eric_broberg@hotmail.com>
2015-12-02 20:25:26 -0800

    Passed spec tests for numbers, null, booleans, strings and arrays.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,34 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if(typeof obj === 'number'){
+  	return obj.toString();
+  }
+
+  if(obj === null){
+  	return 'null';
+  }
+
+  if(typeof obj === 'boolean'){
+  	return obj.toString();
+  }
+
+  if(typeof obj === 'string'){ // Strings get double quotes added as characters to their string.
+  	return '"'+obj+'"';
+  }
+
+  if(Array.isArray(obj)){ // Arrays get stringified.
+
+  	 // Add double quotes to strings in arrays.
+  	 for(var i = 0; i < obj.length; i++){
+  	 	obj[i] = stringifyJSON(obj[i])
+  	 }
+
+  	// Stringify the array, then add brackets as strings.
+
+  	var arrayStringified = obj.toString();
+  	return '['+arrayStringified+']';
+  }
+  
 };
